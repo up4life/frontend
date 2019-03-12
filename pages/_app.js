877efabbceec0b9240bd7efd3bc8fa62c484ced4
Holden B <holden.bucher.92@gmail.com>
2019-03-12 15:25:21 -0500

    trying things

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,36 @@
-import App, { Container } from 'next/app';
-import Router from 'next/router';
-import Page from '../components/Page';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
-import withData from '../utils/withData';
-import redirect from '../utils/redirect';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import JssProvider from 'react-jss/lib/JssProvider';
-import getPageContext from '../utils/getPageContext';
-import '../static/scss/material-kit-pro-react.scss';
+import App, { Container } from "next/app";
+import Router from "next/router";
+import Page from "../components/Page";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
+import withData from "../utils/withData";
+import redirect from "../utils/redirect";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import JssProvider from "react-jss/lib/JssProvider";
+import getPageContext from "../utils/getPageContext";
+import "../static/scss/material-kit-pro-react.scss";
 class MyApp extends App {
 	constructor() {
 		super();
 		this.pageContext = getPageContext();
 	}
 	// pageContext = null;
-	componentDidMount() {
-		const jssStyles = document.querySelector('#jss-server-side');
-		if (jssStyles && jssStyles.parentNode) {
-			jssStyles.parentNode.removeChild(jssStyles);
-		}
-	}
+	// componentDidMount() {
+	// 	const jssStyles = document.querySelector('#jss-server-side');
+	// 	if (jssStyles && jssStyles.parentNode) {
+	// 		jssStyles.parentNode.removeChild(jssStyles);
+	// 	}
+	// }
 	static async getInitialProps({ Component, ctx, router }) {
 		//console.log(Object.keys(ctx));
 		let pageProps = {};
 		if (Component.getInitialProps) {
+			console.log(!!ctx.req);
 			pageProps = await Component.getInitialProps(ctx);
+			if (!process.browser) {
+				console.log(pageProps, "pageProps");
+			}
 		}
 
 		pageProps.query = ctx.query;
