7fc856d32ec13297917fd9b7b63464aea77b173a
Holden B <holden.bucher.92@gmail.com>
2019-03-12 23:14:03 -0500

    pow

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ export const prodEndpoint = `http://api.up4.life`;
 export const appUrl = `http://localhost:3000`;
 export const prodAppUrl = `https://app.up4.life`;
 export const wsEndpoint = "ws://localhost:4000";
-export const wsProdEndpoint = "ws://api.up4.life";
+export const wsProdEndpoint = "wss://api.up4.life";
