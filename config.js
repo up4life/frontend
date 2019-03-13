e6b02355107054219ccf1efac0f4dd43f5b0ad56
Holden B <holden.bucher.92@gmail.com>
2019-03-12 23:09:24 -0500

    glub

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const endpoint = `http://localhost:4000`;
-export const prodEndpoint = `https://api.up4.life`;
+export const prodEndpoint = `http://api.up4.life`;
 export const appUrl = `http://localhost:3000`;
 export const prodAppUrl = `https://app.up4.life`;
 export const wsEndpoint = "ws://localhost:4000";
-export const wsProdEndpoint = "wss://api.up4.life";
+export const wsProdEndpoint = "ws://api.up4.life";
