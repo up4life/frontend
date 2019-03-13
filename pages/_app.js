1f8d038ab05e84120b871ec0fd606120997f5242
Holden B <holden.bucher.92@gmail.com>
2019-03-13 12:48:52 -0500

    new stuffs

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
-import App, { Container } from "next/app";
-import Router from "next/router";
-import Page from "../components/Page";
-import { ApolloProvider } from "react-apollo";
 import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
-import withData from "../utils/withData";
-import redirect from "../utils/redirect";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import JssProvider from "react-jss/lib/JssProvider";
 import getPageContext from "../utils/getPageContext";
+import JssProvider from "react-jss/lib/JssProvider";
+import { ApolloProvider } from "react-apollo";
+import App, { Container } from "next/app";
+import nextCookie from "next-cookies";
+// import Router from "next/router";
+// import redirect from "../utils/redirect";
 import "../static/scss/material-kit-pro-react.scss";
+import withData from "../utils/withData";
+import Page from "../components/Page";
+
 class MyApp extends App {
 	constructor(props) {
 		super(props);
@@ -26,6 +28,8 @@ class MyApp extends App {
 		if (Component.getInitialProps) {
 			pageProps = await Component.getInitialProps(ctx);
 		}
+		const tokenObj = nextCookie(ctx);
+		console.log(tokenObj);
 		// this exposes the query to the user
 		pageProps.query = ctx.query;
 		return { pageProps };
