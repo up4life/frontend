0a5d93438edffbca27da60e50b356ede55f3fd7a
Lauren <llwor94@gmail.com>
2019-03-19 14:44:07 -0700

    ok

--- utils/withApollo.js
+++ utils/withApollo.js
@@ -40,30 +40,30 @@ export default App => {
 				return {};
 			}
 
-			if (!process.browser) {
-				// Run all graphql queries in the component tree
-				// and extract the resulting data
-				try {
-					// Run all GraphQL queries
-					await getDataFromTree(
-						<App
-							{...appProps}
-							Component={Component}
-							router={router}
-							apolloClient={apollo}
-						/>,
-					);
-				} catch (error) {
-					// Prevent Apollo Client GraphQL errors from crashing SSR.
-					// Handle them in components via the data.error prop:
-					// https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
-					console.error('Error while running `getDataFromTree`', error);
-				}
-
-				// getDataFromTree does not call componentWillUnmount
-				// head side effect therefore need to be cleared manually
-				Head.rewind();
+			//if (!process.browser) {
+			// Run all graphql queries in the component tree
+			// and extract the resulting data
+			try {
+				// Run all GraphQL queries
+				await getDataFromTree(
+					<App
+						{...appProps}
+						Component={Component}
+						router={router}
+						apolloClient={apollo}
+					/>,
+				);
+			} catch (error) {
+				// Prevent Apollo Client GraphQL errors from crashing SSR.
+				// Handle them in components via the data.error prop:
+				// https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
+				console.error('Error while running `getDataFromTree`', error);
 			}
+
+			// getDataFromTree does not call componentWillUnmount
+			// head side effect therefore need to be cleared manually
+			Head.rewind();
+			//}
 
 			// Extract query data from the Apollo's store
 			const apolloState = apollo.cache.extract();
