fdb853b95466117cab3039b9452ee80a6d53cab4
Holden B <holden.bucher.92@gmail.com>
2019-03-19 20:35:41 -0500

    rah

--- pages/index.js
+++ pages/index.js
@@ -19,15 +19,20 @@ const Index = () => (
 Index.getInitialProps = async ctx => {
 	if (!process.browser) {
 		if (ctx.req && ctx.req.headers) {
-			console.log(Object.keys(ctx.req));
+			// console.log(Object.keys(ctx.req));
+			console.log(ctx.req.client, "req.client");
+			console.log(ctx.req.query, "req.query");
+			console.log(ctx.req.body, "ctx.req.body");
+			console.log(ctx.req.cookies, "cookies");
+			console.log(ctx.req.method, "http method");
+			console.log(ctx.req.statusMessage, "http status message");
+			console.log(ctx.req.route, "http route");
 			// let user = await isLoggedIn(ctx.apolloClient);
 			// console.log("init props", user);
-			let response = await ctx.apolloClient.query({
+			const response = await ctx.apolloClient.query({
 				query: CURRENT_USER_QUERY,
 				context: {
-					fetchOptions: {
-						credentials: "include"
-					}
+					credentials: "include"
 				}
 				//operationName: 'hellayyy',
 				// fetchPolicy: "cache-and-network"
