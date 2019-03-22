b351435c88c33df1061d80697bf3316b03e77388
Holden B <holden.bucher.92@gmail.com>
2019-03-22 17:14:55 -0500

    ready

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const next = require("next");
 const path = require("path");
 const port = parseInt(process.env.PORT, 10) || 3000;
-const app = next({ dev: process.env.NODE_ENV !== "production" });
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
