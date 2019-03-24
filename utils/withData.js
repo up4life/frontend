607c40e55b842cdfe6903732ee378470cc20cbcd
Lauren <llwor94@gmail.com>
2019-03-24 14:43:44 -0700

    ok

--- utils/withData.js
+++ utils/withData.js
@@ -1,36 +1,39 @@
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { getMainDefinition } from "apollo-utilities";
-import { createHttpLink, HttpLink } from "apollo-link-http";
-import { setContext } from "apollo-link-context";
-import { ApolloLink, split } from "apollo-link";
-import { WebSocketLink } from "apollo-link-ws";
-import { onError } from "apollo-link-error";
-import withApollo from "next-with-apollo";
-import ApolloClient from "apollo-client";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { getMainDefinition } from 'apollo-utilities';
+import { createHttpLink, HttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { ApolloLink, split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { onError } from 'apollo-link-error';
+import withApollo from 'next-with-apollo';
+import ApolloClient from 'apollo-client';
 
-import { endpoint, prodEndpoint, wsEndpoint, wsProdEndpoint } from "../config";
+import { endpoint, prodEndpoint, wsEndpoint, wsProdEndpoint } from '../config';
 
 export default withApollo(({ headers }) => {
 	const ssrMode = !process.browser;
-
+	console.log('headers withData', headers);
 	const httpLink = createHttpLink({
-		uri: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://api.up4.life"
+		uri:
+			process.env.NODE_ENV === 'development'
+				? 'http://localhost:4000'
+				: 'https://api.up4.life',
 	});
 
 	const wsLink =
 		!ssrMode &&
 		new WebSocketLink({
-			uri: process.env.NODE_ENV === "development" ? wsEndpoint : wsProdEndpoint,
+			uri: process.env.NODE_ENV === 'development' ? wsEndpoint : wsProdEndpoint,
 			options: {
-				reconnect: true
-			}
+				reconnect: true,
+			},
 		});
 
 	const contextLink = setContext(() => ({
 		fetchOptions: {
-			credentials: "include"
+			credentials: 'include',
 		},
-		headers
+		headers,
 	}));
 
 	// const middlewareLink = new ApolloLink((operation, forward) => {
@@ -54,27 +57,30 @@ export default withApollo(({ headers }) => {
 		if (networkError) console.log(`[Network error]: ${networkError}`);
 	});
 
-	let link = ApolloLink.from([errorLink, contextLink, httpLink]);
+	let link = ApolloLink.from([ errorLink, contextLink, httpLink ]);
 
 	if (!ssrMode) {
 		link = split(
 			// split based on operation type
 			({ query }) => {
 				const definition = getMainDefinition(query);
-				return definition.kind === "OperationDefinition" && definition.operation === "subscription";
+				return (
+					definition.kind === 'OperationDefinition' &&
+					definition.operation === 'subscription'
+				);
 			},
 			wsLink,
-			link
+			link,
 		);
 	}
 
 	const cache = new InMemoryCache({
-		dataIdFromObject: ({ id, __typename }) => (id && __typename ? __typename + id : null)
+		dataIdFromObject: ({ id, __typename }) => (id && __typename ? __typename + id : null),
 	});
 
 	return new ApolloClient({
 		link,
 		ssrMode,
-		cache
+		cache,
 	});
 });
