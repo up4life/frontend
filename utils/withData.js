0da07a674ffad36068b6689c5b0a9d6e1fe414c1
Lauren <llwor94@gmail.com>
2019-03-24 15:22:34 -0700

    ok

--- utils/withData.js
+++ utils/withData.js
@@ -10,80 +10,83 @@ import ApolloClient from 'apollo-client';
 
 import { endpoint, prodEndpoint, wsEndpoint, wsProdEndpoint } from '../config';
 
-export default withApollo(({ headers = {} }) => {
-	const ssrMode = !process.browser;
-	console.log('headers withData', headers);
-	const httpLink = createHttpLink({
-		uri:
-			process.env.NODE_ENV === 'development'
-				? 'http://localhost:4000'
-				: 'https://api.up4.life',
-	});
-
-	const wsLink =
-		!ssrMode &&
-		new WebSocketLink({
-			uri: process.env.NODE_ENV === 'development' ? wsEndpoint : wsProdEndpoint,
-			options: {
-				reconnect: true,
-			},
+export default withApollo(
+	({ headers = {} }) => {
+		const ssrMode = !process.browser;
+		console.log('headers withData', headers);
+		const httpLink = createHttpLink({
+			uri:
+				process.env.NODE_ENV === 'development'
+					? 'http://localhost:4000'
+					: 'https://api.up4.life',
 		});
 
-	const contextLink = setContext(() => ({
-		fetchOptions: {
-			credentials: 'include',
-		},
-		headers: {
-			...headers,
-			cookie: headers && headers.cookie,
-		},
-	}));
+		const wsLink =
+			!ssrMode &&
+			new WebSocketLink({
+				uri: process.env.NODE_ENV === 'development' ? wsEndpoint : wsProdEndpoint,
+				options: {
+					reconnect: true,
+				},
+			});
 
-	// const middlewareLink = new ApolloLink((operation, forward) => {
-	// 	return forward(operation).map(response => {
-	// 		const context = operation.getContext();
-
-	// 		if (context.req && context.req.headers) {
-	// 			const cookie = context.req.headers.get("set-cookie");
-	// 			if (cookie) {
-	// 				console.log(cookie);
-	// 			}
-	// 		}
-	// 		return response;
-	// 	});
-	// });
-
-	const errorLink = onError(({ graphQLErrors, networkError }) => {
-		if (graphQLErrors) {
-			graphQLErrors.map(err => console.log(`[GraphQL error]: Message: ${err.message}`));
-		}
-		if (networkError) console.log(`[Network error]: ${networkError}`);
-	});
-
-	let link = ApolloLink.from([ errorLink, contextLink, httpLink ]);
-
-	if (!ssrMode) {
-		link = split(
-			// split based on operation type
-			({ query }) => {
-				const definition = getMainDefinition(query);
-				return (
-					definition.kind === 'OperationDefinition' &&
-					definition.operation === 'subscription'
-				);
+		const contextLink = setContext(() => ({
+			fetchOptions: {
+				credentials: 'include',
 			},
-			wsLink,
+			headers: {
+				...headers,
+				cookie: headers && headers.cookie,
+			},
+		}));
+
+		// const middlewareLink = new ApolloLink((operation, forward) => {
+		// 	return forward(operation).map(response => {
+		// 		const context = operation.getContext();
+
+		// 		if (context.req && context.req.headers) {
+		// 			const cookie = context.req.headers.get("set-cookie");
+		// 			if (cookie) {
+		// 				console.log(cookie);
+		// 			}
+		// 		}
+		// 		return response;
+		// 	});
+		// });
+
+		const errorLink = onError(({ graphQLErrors, networkError }) => {
+			if (graphQLErrors) {
+				graphQLErrors.map(err => console.log(`[GraphQL error]: Message: ${err.message}`));
+			}
+			if (networkError) console.log(`[Network error]: ${networkError}`);
+		});
+
+		let link = ApolloLink.from([ errorLink, contextLink, httpLink ]);
+
+		if (!ssrMode) {
+			link = split(
+				// split based on operation type
+				({ query }) => {
+					const definition = getMainDefinition(query);
+					return (
+						definition.kind === 'OperationDefinition' &&
+						definition.operation === 'subscription'
+					);
+				},
+				wsLink,
+				link,
+			);
+		}
+
+		const cache = new InMemoryCache({
+			dataIdFromObject: ({ id, __typename }) => (id && __typename ? __typename + id : null),
+		});
+
+		return new ApolloClient({
 			link,
-		);
-	}
-
-	const cache = new InMemoryCache({
-		dataIdFromObject: ({ id, __typename }) => (id && __typename ? __typename + id : null),
-	});
-
-	return new ApolloClient({
-		link,
-		ssrMode,
-		cache,
-	});
-});
+			ssrMode,
+			cache,
+		});
+	},
+	{ getDataFromTree: 'always' },
+);
