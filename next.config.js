15c6863562756b23276bff3180a58a5bc2451948
Holden B <holden.bucher.92@gmail.com>
2019-04-05 13:49:08 -0500

    that should do it

--- next.config.js
+++ next.config.js
@@ -1,25 +1,23 @@
 require('dotenv').config();
 const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
-const withOffline = require('next-offline');
+// const withOffline = require('next-offline');
 
 const withAssetsImport = require('next-assets-import');
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = withOffline(
-	withSass(
-		withImages({
-			publicRuntimeConfig: {
-				endpoint: process.env.ENDPOINT,
-				wsEndpoint: process.env.WSENDPOINT,
-				firebase_key: process.env.FIREBASE_KEY,
-				firebase_domain: process.env.FIREBASE_DOMAIN,
-				firebase_url: process.env.FIREBASE_URL,
-				firebase_id: process.env.FIREBASE_ID,
-				firebase_bucket: process.env.FIREBASE_BUCKET,
-				firebase_msg_id: process.env.FIREBASE_MSG_ID,
-				stripe_key: process.env.STRIPE
-			}
-		})
-	)
+module.exports = withSass(
+	withImages({
+		publicRuntimeConfig: {
+			endpoint: process.env.ENDPOINT,
+			wsEndpoint: process.env.WSENDPOINT,
+			firebase_key: process.env.FIREBASE_KEY,
+			firebase_domain: process.env.FIREBASE_DOMAIN,
+			firebase_url: process.env.FIREBASE_URL,
+			firebase_id: process.env.FIREBASE_ID,
+			firebase_bucket: process.env.FIREBASE_BUCKET,
+			firebase_msg_id: process.env.FIREBASE_MSG_ID,
+			stripe_key: process.env.STRIPE
+		}
+	})
 );
